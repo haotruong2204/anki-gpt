88d3b58fbd781c08250822bab30c2a263021e37b
Hao Truong <haotv03@gmail.com>
2023-10-19 22:48:22 +0700

    fix: file production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@ set :stage, :production
 set :rails_env, :production
 set :rack_env, :production
 set :branch, 'main'
-set :deploy_to, '/home/ubuntu/anki-gpt'
+set :deploy_to, '/var/www/anki-gpt'
 
 server "122.248.207.27", user: "ubuntu", roles: %w(web app db)
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,6 @@ set :stage, :staging
 set :rails_env, :staging
 set :rack_env, :staging
 set :branch, 'main'
-set :deploy_to, '/home/ubuntu/anki-gpt'
+set :deploy_to, '/var/www/anki-gpt'
 
 server "122.248.207.27", user: "ubuntu", roles: %w(web app db)
